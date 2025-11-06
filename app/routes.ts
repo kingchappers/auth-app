aba5d9285ca57c219b574e4fc56ec160fe28e7dc
kingchappers <sam.chapman@outlook.com>
2025-11-06 16:50:10 +0000

    added fs-routes and config

--- app/routes.ts
+++ app/routes.ts
@@ -1,10 +1,6 @@
 // Documentation for route configuration can be found at: https://reactrouter.com/how-to/file-route-conventions
 
-import {
-    type RouteConfig,
-    index
-} from "@react-router/dev/routes";
+import { type RouteConfig } from "@react-router/dev/routes";
+import { flatRoutes } from "@react-router/fs-routes";
 
-export default [
-    index("routes/home.tsx")
-] satisfies RouteConfig;
+export default flatRoutes() satisfies RouteConfig;
