c88ea996809b5659345fc94897ce610554151dfd
kingchappers <sam.chapman@outlook.com>
2025-11-06 08:01:31 +0000

    created the Router project using their basic project creation

---
+++ app/routes.ts
@@ -0,0 +1,10 @@
+// Documentation for route configuration can be found at: https://reactrouter.com/how-to/file-route-conventions
+
+import {
+    type RouteConfig,
+    index
+} from "@react-router/dev/routes";
+
+export default [
+    index("routes/home.tsx")
+] satisfies RouteConfig;
