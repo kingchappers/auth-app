67b241939b984aa6d49b62bd7a4f852ad864f350
kingchappers <sam.chapman@outlook.com>
2025-11-08 15:29:47 +0000

    introduced authentication via auth0

---
+++ app/routes/test.tsx
@@ -0,0 +1,5 @@
+import { Welcome } from "../welcome/welcome";
+
+export default function Home() {
+    return <p>The test has worked and you've created a new route to a test page under /test</p>
+}
--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,48 +1,66 @@
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
+import { StrictMode } from 'react';
+import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
+import Authentication from "~/authentication/authentication";
 
 export function Welcome() {
+
   return (
-    <main className="flex items-center justify-center pt-16 pb-4">
-      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
-        <header className="flex flex-col items-center gap-9">
-          <div className="w-[500px] max-w-[100vw] p-4">
-            <img
-              src={logoLight}
-              alt="React Router"
-              className="block w-full dark:hidden"
-            />
-            <img
-              src={logoDark}
-              alt="React Router"
-              className="hidden w-full dark:block"
-            />
+
+    <StrictMode>
+      <Auth0Provider
+        domain={import.meta.env.VITE_AUTH0_DOMAIN}
+        clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+        authorizationParams={{
+          redirect_uri: window.location.origin
+        }}
+      >
+        <Authentication />
+      </Auth0Provider>
+
+      <main className="flex items-center justify-center pt-16 pb-4">
+        <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
+          <header className="flex flex-col items-center gap-9">
+            <div className="w-[500px] max-w-[100vw] p-4">
+              <img
+                src={logoLight}
+                alt="React Router"
+                className="block w-full dark:hidden"
+              />
+              <img
+                src={logoDark}
+                alt="React Router"
+                className="hidden w-full dark:block"
+              />
+            </div>
+          </header>
+          <div className="max-w-[300px] w-full space-y-6 px-4">
+            <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
+              <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
+                What's next?
+              </p>
+              <ul>
+                {resources.map(({ href, text, icon }) => (
+                  <li key={href}>
+                    <a
+                      className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
+                      href={href}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {icon}
+                      {text}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </nav>
           </div>
-        </header>
-        <div className="max-w-[300px] w-full space-y-6 px-4">
-          <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
-            <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
-              What&apos;s next?
-            </p>
-            <ul>
-              {resources.map(({ href, text, icon }) => (
-                <li key={href}>
-                  <a
-                    className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
-                    href={href}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {icon}
-                    {text}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </nav>
         </div>
-      </div>
-    </main>
+      </main>
+
+    </StrictMode>
   );
 }
 
